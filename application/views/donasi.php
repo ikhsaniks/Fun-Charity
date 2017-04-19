6607dc744695e3510e6cba68a929224909153ec2
komandancup <yusufsuf@gmail.com>
2017-04-19 13:50:44 +0700

    Fixing sample project

--- application/views/donasi.php
+++ application/views/donasi.php
@@ -10,10 +10,13 @@
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('file/bootstrap/css/font-awesome.css') ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('file/bootstrap/css/demo.css') ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('file/bootstrap/font-awesome-ie7.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('file/bootstrap/css/bootstrap2.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('file/bootstrap/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('file/bootstrap/css/3-col-portfolio') ?>">
     
-     <script src="<?php echo base_url('file/bootstrap/js/jquery.mobilemenu.js') ?>"></script>
-     <script src="<?php echo base_url('file/bootstrap/js/html5shiv.js') ?>"></script>
-     <script src="<?php echo base_url('file/bootstrap/js/respond.min.js') ?>"></script>
+     <script src="<?php echo base_url('file/bootstrap/js/bootstrap.min.js') ?>"></script>
+     <script src="<?php echo base_url('file/bootstrap/js/jquery.js') ?>"></script>
+     <script src="<?php echo base_url('file/bootstrap/js/bootsrap.js') ?>"></script>
     
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('file/mdl/material.css') ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('file/mdl/material.min.css') ?>">
@@ -29,7 +32,8 @@
   <header class="mdl-layout__header" style="background-color:#5E35B1;">
     <div class="mdl-layout__header-row">
       <!-- Title -->
-      <span class="mdl-layout-title"><img src="<?php echo base_url('file/asset/img/FunCharityLogo.png') ?>" alt="" class="img-responsive" width="40px" height="40px"> Fun Charity</span>
+	  <img src="<?php echo base_url('file/asset/img/FunCharityLogo.png') ?>" alt="" class="img-responsive" width="40px" height="40px">
+      <span class="mdl-layout-title"  > Fun Charity</span>
       <!-- Add spacer, to align navigation to the right -->
       <div class="mdl-layout-spacer"></div>
       <!-- Navigation. We hide it in small screens. -->
@@ -45,12 +49,206 @@
     <div class="page-content">
     <!-- Container -->
         <div class="container" align="center">
-                    <div class="col-sm-8">
-                      <h2>FUN CHARITY</h2>
-                      <img src="<?php echo base_url('file/asset/img/banner1.jpg') ?>" alt="" style="width:600px; height:250px;">
-                      <h4><strong>MISSI:</strong> Membantu kaum yang kurang beruntung atau yang kurang mampu</h4>      
-                      <p><strong>VISI:</strong> Penggalangan Dana Online , dari teman-teman yang beruntung</p>
+                    <!-- Page Header -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Donasi 
+                    <small>Tidak akan membuatmu miskin</small>
+                </h1>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Projects Row -->
+        <div class="featured-block">
+        <!-- Example row of columns -->
+        <div class="row">
+          <div class="col-md-3">
+            <div class="block">
+            <div class="thumbnail">
+              <img src="<?php echo base_url('file/asset/img/tanahlongsor.jpg') ?>" alt="" style="width:360px; height:170px;">
+              <div class="caption">
+                <h1>Bencana Alam</h1>
+                <p>Cuibstui ipsum dolor sit amet, consectetuer adipiscing eli onec odio cuisque volutpat mattis</p>
+                <a class="btn" href="<?php echo base_url('index.php/beranda/detail') ?>">more</a>
+              </div>
+              </div>
+            </div>
+            </div>
+            <div class="col-md-3">
+              <div class="block">
+                <div class="thumbnail">
+                    <img src="<?php echo base_url('file/asset/img/img2.jpg') ?>" alt="" class="img-responsive">
+                    <div class="caption">
+                      <h1>Kaum Duafa 	</h1>
+                      <p>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis ctor, ultrices ut, element</p>
+                      <a class="btn" href="#">more</a>
                     </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="block">
+                <div class="thumbnail">
+                  <img src="<?php echo base_url('file/asset/img/img3.jpg') ?>" alt="" class="img-responsive">
+                  <div class="caption">
+                    <h1>Penyembuhan Penyakit</h1>
+                    <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean</p>
+                    <a class="btn" href="#">more</a>
+                  </div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+            <div class="block">
+            <div class="thumbnail">
+              <img src="<?php echo base_url('file/asset/img/img4.jpg') ?>" alt="" class="img-responsive">
+              <div class="caption">
+                <h1>Pembangunan Tempat Ibadah</h1>
+                <p>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, mod in, pharetra ultrici</p>
+                <a class="btn" href="#">more</a>
+              </div>
+              </div>
+            </div>
+            </div>
+            </div>
+        </div><!-- /.row -->
+
+        <!-- Projects Row -->
+		 <div class="featured-block">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="block">
+            <div class="thumbnail">
+              <img src="<?php echo base_url('file/asset/img/tanahlongsor.jpg') ?>" alt="" style="width:360px; height:170px;">
+              <div class="caption">
+                <h1>Bencana Alam</h1>
+                <p>Cuibstui ipsum dolor sit amet, consectetuer adipiscing eli onec odio cuisque volutpat mattis</p>
+                <a class="btn" href="#">more</a>
+              </div>
+              </div>
+            </div>
+            </div>
+            <div class="col-md-3">
+              <div class="block">
+                <div class="thumbnail">
+                    <img src="<?php echo base_url('file/asset/img/img2.jpg') ?>" alt="" class="img-responsive">
+                    <div class="caption">
+                      <h1>Kaum Duafa 	</h1>
+                      <p>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis ctor, ultrices ut, element</p>
+                      <a class="btn" href="#">more</a>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="block">
+                <div class="thumbnail">
+                  <img src="<?php echo base_url('file/asset/img/img3.jpg') ?>" alt="" class="img-responsive">
+                  <div class="caption">
+                    <h1>Penyembuhan Penyakit</h1>
+                    <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean</p>
+                    <a class="btn" href="#">more</a>
+                  </div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+            <div class="block">
+            <div class="thumbnail">
+              <img src="<?php echo base_url('file/asset/img/img4.jpg') ?>" alt="" class="img-responsive">
+              <div class="caption">
+                <h1>Pembangunan Tempat Ibadah</h1>
+                <p>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, mod in, pharetra ultrici</p>
+                <a class="btn" href="#">more</a>
+              </div>
+              </div>
+            </div>
+            </div>
+          </div>
+		 </div>
+
+        <!-- Projects Row -->
+        <div class="featured-block">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="block">
+            <div class="thumbnail">
+              <img src="<?php echo base_url('file/asset/img/tanahlongsor.jpg') ?>" alt="" style="width:360px; height:170px;">
+              <div class="caption">
+                <h1>Bencana Alam</h1>
+                <p>Cuibstui ipsum dolor sit amet, consectetuer adipiscing eli onec odio cuisque volutpat mattis</p>
+                <a class="btn" href="#">more</a>
+              </div>
+              </div>
+            </div>
+            </div>
+            <div class="col-md-3">
+              <div class="block">
+                <div class="thumbnail">
+                    <img src="<?php echo base_url('file/asset/img/img2.jpg') ?>" alt="" class="img-responsive">
+                    <div class="caption">
+                      <h1>Kaum Duafa 	</h1>
+                      <p>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis ctor, ultrices ut, element</p>
+                      <a class="btn" href="#">more</a>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="block">
+                <div class="thumbnail">
+                  <img src="<?php echo base_url('file/asset/img/img3.jpg') ?>" alt="" class="img-responsive">
+                  <div class="caption">
+                    <h1>Penyembuhan Penyakit</h1>
+                    <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean</p>
+                    <a class="btn" href="#">more</a>
+                  </div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+            <div class="block">
+            <div class="thumbnail">
+              <img src="<?php echo base_url('file/asset/img/img4.jpg') ?>" alt="" class="img-responsive">
+              <div class="caption">
+                <h1>Pembangunan Tempat Ibadah</h1>
+                <p>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, mod in, pharetra ultrici</p>
+                <a class="btn" href="#">more</a>
+              </div>
+              </div>
+            </div>
+            </div>
+          </div>
+		 </div>
+		 <!-- Pagination -->
+        <div class="row text-center">
+            <div class="col-lg-12">
+                <ul class="pagination">
+                    <li>
+                        <a href="#">&laquo;</a>
+                    </li>
+                    <li class="active">
+                        <a href="#">1</a>
+                    </li>
+                    <li>
+                        <a href="#">2</a>
+                    </li>
+                    <li>
+                        <a href="#">3</a>
+                    </li>
+                    <li>
+                        <a href="#">4</a>
+                    </li>
+                    <li>
+                        <a href="#">5</a>
+                    </li>
+                    <li>
+                        <a href="#">&raquo;</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
         </div>
          <!-- footer --> 
         <footer class="mdl-mini-footer" style="background-color:#263238">
@@ -61,5 +259,10 @@
     </div>
   </main>
 </div>
+ <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
