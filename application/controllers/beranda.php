dab0efa17763f72ba88e3ade73e8120d8dfe1113
ikhsaniks <miksya@gmail.com>
2017-04-19 12:31:08 +0700

    Commit Pertama Fun Charity

---
+++ application/controllers/beranda.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class beranda extends CI_Controller{
+    
+    function __construct(){
+		parent:: __construct();
+		$this->load->helper('url');
+	}
+    
+    public function index(){
+        $this->load->view('Welcome Message');
+        $this->load->helper('url');
+    }
+    public function home(){
+        $this->load->view('index');
+    }
+    
+    public function donasi(){
+        $this->load->view('donasi');
+    }
+    
+    public function aboutus(){
+        $this->load->view('aboutus');
+    }
+}
+
+?>
